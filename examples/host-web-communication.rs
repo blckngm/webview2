d10a80bdab3e7673cdadd3914128fc7356b4f829
Yin Guanhao <sopium@mysterious.site>
2020-09-16 22:50:26 +0800

    Doc

--- examples/host-web-communication.rs
+++ examples/host-web-communication.rs
@@ -10,6 +10,9 @@ use winit::platform::windows::WindowExtWindows;
 use winit::window::WindowBuilder;
 
 fn main() {
+    // First we need to create an window. Here we use winit, but you can also
+    // use other crates, or use raw windows APIs. Just make sure that you can
+    // have the HWND, and can handle window messages, esp. WM_SIZE.
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
         .with_title("WebView2 - Host Web Communication")
@@ -17,38 +20,46 @@ fn main() {
         .build(&event_loop)
         .unwrap();
 
-    let webview_host: Rc<RefCell<Option<webview2::Controller>>> = Rc::new(RefCell::new(None));
+    let webview_controller: Rc<RefCell<Option<webview2::Controller>>> = Rc::new(RefCell::new(None));
     let webview: Rc<RefCell<Option<webview2::WebView>>> = Rc::new(RefCell::new(None));
 
     let create_result = {
-        let webview_host = webview_host.clone();
+        let webview_controller = webview_controller.clone();
         let hwnd = window.hwnd() as HWND;
 
+        // Create an webview2 environment.
         webview2::EnvironmentBuilder::new().build(move |env| {
-            env.expect("env").create_controller(hwnd, move |host| {
-                let host = host.expect("create host");
-                let w = host.get_webview().expect("get_webview");
+            // Then we create the controller. Here we need the HWND.
+            env.expect("env").create_controller(hwnd, move |controller| {
+                let controller = controller.expect("create host");
+                // Then we get the WebView.
+                let w = controller.get_webview().expect("get_webview");
 
+                // We can manipulate settings of the webview.
                 let _ = w.get_settings().map(|settings| {
                     let _ = settings.put_is_status_bar_enabled(false);
                     let _ = settings.put_are_default_context_menus_enabled(false);
                     let _ = settings.put_is_zoom_control_enabled(false);
                 });
 
+                // Tell the controller it should occupy the whole parent window.
                 unsafe {
                     let mut rect = mem::zeroed();
                     GetClientRect(hwnd, &mut rect);
-                    host.put_bounds(rect).expect("put_bounds");
+                    controller.put_bounds(rect).expect("put_bounds");
                 }
 
+                // Make the webview display some HTML.
                 w.navigate_to_string("<h2>WebView2 - Host Web Communication</h2><script>window.chrome.webview.postMessage('hello from web-view!')</script>").expect("navigate to string");
+                // Host-web communication. Add a callback for web messages.
                 w.add_web_message_received(|w, args| {
                     let message = args.try_get_web_message_as_string()?;
                     println!("Message from webview: {}", message);
+                    // Execute some script in the webview.
                     w.execute_script("document.write('<h2>WebView2 - Host Web Communication</h2><p>I got your message!</p>')", |_| Ok(()))
                 }).expect("add_web_message_received");
 
-                *webview_host.borrow_mut() = Some(host);
+                *webview_controller.borrow_mut() = Some(controller);
                 *webview.borrow_mut() = Some(w);
                 Ok(())
             })
@@ -67,20 +78,20 @@ fn main() {
         match event {
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::CloseRequested => {
-                    if let Some(ref webview_host) = webview_host.borrow().as_ref() {
+                    if let Some(ref webview_host) = webview_controller.borrow().as_ref() {
                         webview_host.close().expect("close");
                     }
                     *control_flow = ControlFlow::Exit;
                 }
                 // Notify the webview when the parent window is moved.
                 WindowEvent::Moved(_) => {
-                    if let Some(ref webview_host) = webview_host.borrow().as_ref() {
+                    if let Some(ref webview_host) = webview_controller.borrow().as_ref() {
                         let _ = webview_host.notify_parent_window_position_changed();
                     }
                 }
                 // Update webview bounds when the parent window is resized.
                 WindowEvent::Resized(new_size) => {
-                    if let Some(ref webview_host) = webview_host.borrow().as_ref() {
+                    if let Some(ref webview_host) = webview_controller.borrow().as_ref() {
                         let r = RECT {
                             left: 0,
                             top: 0,
@@ -100,13 +111,13 @@ fn main() {
                 // visibility of the webview to SC_MINIMIZE and SC_RESTORE
                 // system commands.
                 if input.virtual_keycode == Some(winit::event::VirtualKeyCode::S) {
-                    if let Some(ref webview_host) = webview_host.borrow().as_ref() {
+                    if let Some(ref webview_host) = webview_controller.borrow().as_ref() {
                         webview_host
                             .put_is_visible(true)
                             .expect("put_is_visible true");
                     }
                 } else if input.virtual_keycode == Some(winit::event::VirtualKeyCode::H) {
-                    if let Some(ref webview_host) = webview_host.borrow().as_ref() {
+                    if let Some(ref webview_host) = webview_controller.borrow().as_ref() {
                         webview_host
                             .put_is_visible(false)
                             .expect("put_is_visible false");
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,59 @@
-//! Rust bindings for
-//! [WebView2](https://docs.microsoft.com/en-us/microsoft-edge/hosting/webview2).
-//!
-//! # API
-//!
-//! There are high level, idiomatic Rust wrappers for most APIs. And there are
-//! bindings to almost all the raw COM APIs in the `webview2-sys` crate. You can
-//! use the `as_inner` methods to convert to raw COM objects and call all those
-//! methods. The `callback` macro can be helpful for implementing callbacks as
-//! COM objects.
-//!
-//! There are some examples in the examples directory.
-//!
-//! # Runtime Requirement
-//!
-//! The Edge browser from beta, dev or canary channels (>= 86.0.622.0) or the
-//! [Evergreen WebView2
-//! Runtime](https://docs.microsoft.com/en-us/microsoft-edge/webview2/concepts/distribution#understand-the-webview2-runtime-and-installer-preview)
-//! need to be installed for this to actually work. Or the
-//! [`build`](struct.EnvironmentBuilder.html#method.build) method will return an
-//! error.
-//!
-//! # Build Environment Requirement and Quirks
-//!
-//! Minimum supported rust version (MSRV) is 1.43.0.
-//!
-//! When using the MSVC toolchain the `WebView2LoaderStatic.lib` is statically
-//! linked. Since the lib seem to be built with visual studio 2019, you need to
-//! use a linker from visual studio 2019.
-//!
-//! When using the GNU toolchain the `WebView2Loader.dll` file need to be loaded
-//! at runtime.
+#![doc = r###"
+Rust bindings for
+[WebView2](https://docs.microsoft.com/en-us/microsoft-edge/hosting/webview2):
 
+> The Microsoft Edge WebView2 control enables you to embed web technologies
+(HTML, CSS, and JavaScript) in your native applications. The WebView2 control
+uses Microsoft Edge (Chromium) as the rendering engine to display the web
+content in native applications. With WebView2, you may embed web code in
+different parts of your native application, or build the entire native
+application within a single WebView. For information on how to start building a
+WebView2 application, see [Get
+Started](https://docs.microsoft.com/en-us/microsoft-edge/webview2/#getting-started).
+
+# API
+
+The `webview2` crate contains high-level, idiomatic wrappers for the raw COM
+APIs, which can be found in the `webview2-sys` crate.
+
+The API mapping should be quite straightforward.
+
+The `CreateCoreWebView2EnvironmentWithDetails` function does not have a direct
+equivalent. It is replaced with a nicer `EnvironmentBuilder` API. The
+`GetAvailableCoreWebView2BrowserVersionString` and `CompareBrowserVersions`
+functions are also exposed through the builder.
+
+# Runtime
+
+The Edge browser from beta, dev or canary channels (>= 86.0.622.0) or the
+[Evergreen WebView2
+Runtime](https://docs.microsoft.com/en-us/microsoft-edge/webview2/concepts/distribution#understand-the-webview2-runtime-and-installer-preview)
+need to be installed for this to actually work. Or the
+[`build`](struct.EnvironmentBuilder.html#method.build) method will return an
+error.
+
+# WebView2Loader
+
+A binary library `WebView2Loader` from the WebView2 SDK need to be used, either
+the DLL `WebView2Loader.dll` or the static library `WebView2LoaderStatic.lib`.
+This brings some complexities:
+
+* When using the **gnu** toolchain, the static library does not seem to work so
+  the `WebView2Loader.dll` DLL is used. You need to make sure that the DLL can
+  be loaded at runtime, e.g. by **putting it alongside the built exe files**.
+
+* When using the **msvc** toolchain, the static library is used. Make sure you
+  have the **v142** toolset (or **visual studio 2019**), because the static
+  library seem to be built with visual studio 2019 and could not be correctly
+  linked by earlier versions of the visual studio. See [C++ binary compatibility
+  between Visual Studio 2015, 2017, and
+  2019](https://docs.microsoft.com/en-us/cpp/porting/binary-compat-2015-2017?view=vs-2019).
+
+# Examples
+
+See the `examples` directory, especially the heavily commented
+`host-web-communication` example.
+"###]
 #![cfg(windows)]
 // Caused by the `com_interface` macro.
 #![allow(clippy::cmp_null)]
